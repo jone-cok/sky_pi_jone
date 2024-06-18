4fd0497f8270f69096ac87db67a6ae9bb37d944d
DESKTOP-9NB2H4E\111 <Ofantas@outlook.com>
2024-06-18 02:57:35 -0700

    working

---
+++ src/Components/Loginout/Loginout.js
@@ -0,0 +1,14 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+const Loginout = () => {
+  return (
+    <div className="container">
+      <div className="banner-container">
+        <div>
+          <h2 className="d-flex flex-row-reverse bg-secondary">SignLogin</h2>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Loginout;
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,18 +1,46 @@
-import { Link } from 'react-router-dom';
-
+import { Link } from "react-router-dom";
+import Button from '../Components/Button/Button';
 const Home = () => {
-	return (
-		<div className="container">
-			<div className="banner-container">
-				<div className="banner">
-					<h2>It's time for some Cocktails</h2>
-					<Link to="/products">
-						<div className="btn">View Cocktails</div>
-					</Link>
-				</div>
+  return (
+    <div className="container">
+      <div className="banner-container">
+        <div className="banner">
+          <div style={{ padding: "0px 0px 0px 80%" }}>
+            <Link to="/products">
+              <div
+                style={{
+                  textAlign: "center",
+                  fontSize: "30px",
+                  fontFamily: "Cursive",
+                }}
+              >
+                <div style={{ color: "blue" }}>SignUp/</div>
+                <div style={{ color: "red" }}>Login</div>
+              </div>
+            </Link>
+          </div>
+          <div className="bannerhome">
+			<h5 style={{textAlign:"center"}}>Help me</h5>
+            <div className="bannerimageprofile" >
+            </div>
+			<div style={{
+				  marginTop: "20px",
+                  textAlign: "left",
+                  fontSize: "40px",
+                  fontFamily: "serif",
+                }}>
+				Name
 			</div>
-		</div>
-	);
+			<input type="text"></input>
+			<Button/>
+            <h2 style={{ float: "right" }}>
+              It's time for somesdgfhwrgjhrsj Cocktails
+            </h2>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
